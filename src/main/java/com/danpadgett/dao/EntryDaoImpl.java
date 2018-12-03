2c1bef5af32d89e17c8072831004fdb4e244c33e
Dan Padgett <daniel_padgett@gap.com>
2018-12-03 14:47:19 -0500

    Fixing compile error

--- src/main/java/com/danpadgett/dao/EntryDaoImpl.java
+++ src/main/java/com/danpadgett/dao/EntryDaoImpl.java
@@ -2,7 +2,6 @@ package com.danpadgett.dao;
 
 import java.util.List;
 
-import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
 import com.danpadgett.model.Entry;
@@ -12,7 +11,7 @@ public class EntryDaoImpl extends AbstractDao<Long, Entry> implements EntryDao {
 
 	@Override
 	public List<Entry> findAllEntries() {
-		//NEED TO IMPLEMENT SOLR
+		return null;
 	}
 
 }
